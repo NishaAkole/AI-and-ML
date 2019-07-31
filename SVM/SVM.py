4cd152afc4d4090cd6c103864356be23adc5a8e7
NishaAkole <47945301+NishaAkole@users.noreply.github.com>
2019-07-31 11:19:52 +0530

    Add files via upload

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -1,7 +1,7 @@
-#PCA with SVM in 3D
+#PCA with SVM(Linear Kernel) 2D 
 
 #-----Required Packages-----#
-from sklearn import svm
+from sklearn import svm    			# To fit the svm classifier
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import numpy as np
@@ -9,7 +9,6 @@ import scipy.io as sio
 import matplotlib.pyplot as plt 
 import time
 from mlxtend.plotting import plot_decision_regions
-#from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 #-----Time Requirements-----#
 start_time =time.time()
@@ -22,19 +21,10 @@ data = mat_contents['data_all']
 data = StandardScaler().fit_transform(data)
 
 #-----PCA-----#
-pca = PCA(n_components=3).fit_transform(data)
+#pca1 = PCA(n_components=2)
+pca_2D = PCA(n_components=3).fit_transform(data)
 
-#-----Plot for PCA-----#
-fig = plt.figure()
-ax = fig.add_subplot(111,projection='3d')
-ax.scatter3D(pca[:, 0], pca[:, 1],pca[:,2], marker='o')
-ax.set_xlabel('Prin. Comp 1')
-ax.set_ylabel('Prin. Comp 2')
-ax.set_zlabel('Prin. Comp 3')
-ax.set_title('PCA 3D Plot')  
-plt.show()
-
-X = pca
+X = pca_2D
 
 #-----Creating Labels Array-----#
 y = np.zeros(len(X))
@@ -42,14 +32,32 @@ y[:int(len(X)/2)]=1
 y[int(len(X)/2):]=2
 
 #-----SVC with linear kernel C-Regularization parameter-----#
-svc = svm.SVC(kernel='linear', C=1).fit(X, y)
+svc_lin = svm.SVC(kernel='linear', C=1).fit(X, y)
+	                    
+#-----Plotting SVM Decision Region Boundary-----#
+#plot_decision_regions(X[:,0:2], y.astype(np.integer),svc_lin, legend=2)
+#plt.title('SVM Hyperplane')
+#plt.xlabel('Principal Component 1')
+#plt.ylabel('Principal Component 2')
+
+#-----Plotting SVM with Linear Kernel-----#
+plot = plt.figure()
+ax = plot.add_subplot(111)
+plt.scatter(X[:int(len(X)/2),0],X[:int(len(X)/2),1],c='r',alpha=0.5,label="Target Present")
+plt.scatter(X[int(len(X)/2):,0],X[int(len(X)/2):,1],c='b',alpha=0.5,label="Target Absent")
+plt.xlabel('Principal Component 1')
+plt.ylabel('Principal Component 2')
+plt.title('SVM with Linear Kernel in 2D') 
+plt.legend()
+plt.show()
+
 
 Y = np.zeros([10000,3])
 Y[:int(len(Y)/2),0]=1
 Y[int(len(Y)/2):,0]=2
 
-z = lambda X,Y: (-svc.intercept_[0]-svc.coef_[0][0]*X-svc.coef_[0][1]*Y) / svc.coef_[0][2]
-	                    
+z = lambda X,Y: (-svc_lin.intercept_[0]-svc_lin.coef_[0][0]*X-svc_lin.coef_[0][1]*Y) / svc_lin.coef_[0][2]
+
 #-----Plotting SVM with Linear Kernel and its Decision Region Boundary-----#
 fig = plt.figure()
 ax  = fig.add_subplot(111, projection='3d')
@@ -59,7 +67,7 @@ ax.scatter3D(X[int(len(X)/2):,0],X[int(len(X)/2):,1],X[int(len(X)/2):,2],c='b',a
 ax.set_xlabel('Prin. Comp 1')
 ax.set_ylabel('Prin. Comp 2')
 ax.set_zlabel('Prin. Comp 3')
-ax.set_title('PCA Decision Region Boundary')
+ax.set_title('SVM with Linear Kernel in 3D')
 ax.legend()
 plt.show()
 
